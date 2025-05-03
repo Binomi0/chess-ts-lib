c0093345a3cc2c361a28b58f20898c3ac81b61a6
Binomi0 <adolfo@onrubia.es>
2025-05-03 03:20:59 +0200

    Add tsup

---
+++ README.md
@@ -0,0 +1,16 @@
+# ChessLib
+
+A lightweight chess logic library for validating moves and simulating a chess game in JavaScript.
+
+## Features
+
+- Validate legal moves for all pieces
+- Support for castling, pawn promotion, and capture detection
+- Easily extendable and modular
+
+## Usage
+
+```js
+const { Game, WhitePawn, BlackRook } = require("chess-lib");
+// Create board and play!
+```
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { default as Game } from "./game";
+
+export { default as Piece } from "./piece";
+export * from "./piece";
+
+export { default as Player } from "./player";
+export type { Players } from "./player";
+
+export { default as ChessBoard } from "./chessBoard";
+export * from "./chessBoard";
+
+export * from "./utils/helpers";
+
+export * from "./pieces/bishop";
+export * from "./pieces/king";
+export * from "./pieces/knight";
+export * from "./pieces/pawn";
+export * from "./pieces/queen";
+export * from "./pieces/rook";
