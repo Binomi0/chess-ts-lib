a47cea837533664fca3fbb30a5829bc9d66e9efd
Binomio <onrubia78@gmail.com>
2025-05-06 20:02:10 +0200

    Chore: Configure tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
-  sourcemap: true,
-  clean: true,
-  minify: false,
+  format: ["esm", "iife"], // iife para navegador
+  globalName: "ChessTsLib", // Para formato iife
   dts: true,
-  treeshake: true,
+  clean: true,
+  minify: true,
+  sourcemap: true,
+  target: "es2020",
+  platform: "browser",
+  esbuildOptions(options) {
+    options.define = {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    };
+  },
 });
